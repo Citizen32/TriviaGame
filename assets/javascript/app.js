2f59af08c7265bcd7d8d069240570316299b67c9
David Morales <morgar.creative@gmail.com>
2017-10-18 10:20:48 -0500

    Questions and answers added to HTML file

---
+++ assets/javascript/app.js
@@ -0,0 +1,16 @@
+alert("hello you")
+/*Elements Needed:
+    1. "Start" Button, 
+    2. Timer, 
+    3. Questions, 
+    4. "Submitt" Button,
+
+ On click start game:
+  1. start timer
+  2. launch questions
+
+Questions
+ If user guess = to answer
+ mark true
+ add one point.
+*/
